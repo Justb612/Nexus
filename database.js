123b435af2d7e304a57d870f8560ac8c3d36ba5c
JustB612 <borisz0929@gmail.com>
2021-10-15 17:58:42 +0200

    dsa

--- database.js
+++ database.js
@@ -1,46 +1,34 @@
-/* const backgroundVideoOne = require("./video/ekko.mp4");
-const backgroundVideoTwo = require("./video/garen.mp4");
-const backgroundVideoThree = require("./video/vex.mp4");
-const p1G = require("./video/Garen_0.jpg"); 
-const p2G = require("./video/Garen_1.jpg");
-const p3G = require("./video/Garen_2.jpg");
-const p1V = require("http://localhost:4000/vex1.jpg"); 
-const p2V = require("./video/vex2.jpg");
-const p3V = require("./video/vex3.jpg"); 
-const p2 = require("./video/Ekko_1.jpg");
-const p3 = require("./video/Ekko_2.jpg"); */
-
 const profile2 = {
   name: "Garen",
   headline: "Garen Champion Spotlight",
   headline2: "Hover GAREN SPINNING Basterd",
-  //vidSource: backgroundVideoTwo,
-  //picOne: p1G,
-  //picTwo: p2G,
-  //picThree: p3G,
-  like: false,
+  /*vidSource: "https://drive.google.com/uc?id=1m0ZMIDpgpaB_SoONV42jy_dA_OBHfHK8",
+  picOne: "https://drive.google.com/uc?id=1XTa20F9heKPfCTVn637kAA4uONUGyb29",
+  picTwo: "https://drive.google.com/uc?id=1ruX0MU_I-1J9j9DS_gFFTIolqR9CMram",
+  picThree: "https://drive.google.com/uc?id=1AE4sz3E55NEsslkO3Cv24I6OvH_wlN3u",
+  */like: false,
 };
 
 const profile3 = {
   name: "Vex",
   headline: "Vex Champion Spotlight",
   headline2: "Hover Vex Icons Basterd",
-  //vidSource: backgroundVideoThree,
-  //picOne: p1V,
-  //picTwo: p2V,
-  //picThree: p3V,
-  like: false,
+ /* vidSource: "https://drive.google.com/uc?id=1EouPA4TwfxbbPWHflniKznvOUaKPKyqU",
+  picOne: "https://drive.google.com/uc?id=1NPEHQBrCuSQPkISOaqM5pAOg4NAHXuUr",
+  picTwo: "https://drive.google.com/uc?id=1LggfS8zboSHG1c-sYRtqm5HhRarNsfl2",
+  picThree: "https://drive.google.com/uc?id=1eRTM3FPf-W7y4qKd0MZ5plPUES_V_n64",
+  */like: false,
 };
 
 const profile1 = {
   name: "Ekko",
   headline: "Ekko Champion Spotlight",
   headline2: "Hover Dose Icons Basterd",
-  //vidSource: "ekko.mp4",
-  //picOne: p1,
-  //picTwo: p2,
-  //picThree: p3,
-  like: false,
+  /*vidSource: "https://drive.google.com/uc?id=1xhEoZjtuhSCv3tQaIw6_xdv7Cb9veVWM",
+  picOne: "https://drive.google.com/uc?id=1Rqa36TiuaJrBb6kb7GQf4qTEIB53JusV",
+  picTwo: "https://drive.google.com/uc?id=1rJmpYa9_Ue7VqlVcALZOhgpI6SDl8ndo",
+  picThree: "https://drive.google.com/uc?id=1-se6x0a0SwKfUZcrr7g9t9X2x3OijfqU",
+  */like: false,
 };
 
 const database = [profile1, profile2, profile3];
