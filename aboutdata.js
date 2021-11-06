ff934b55cef66919ca9a769deffe6a48cbe8eea0
JustB612 <borisz0929@gmail.com>
2021-11-06 20:47:06 +0100

    asdasd

---
+++ aboutdata.js
@@ -0,0 +1,28 @@
+aboutdata = [
+  {
+    id: 1,
+    text: "ColdStress",
+    day: "MyDay",
+    reminder: true,
+  },
+  {
+    id: 2,
+    text: "Exercise",
+    day: "Equilibrium",
+    reminder: true,
+  },
+  {
+    id: 3,
+    text: "Sauna",
+    day: "Sunshine",
+    reminder: true,
+  },
+  {
+    id: 4,
+    text: "Mindfulness Practice",
+    day: "Moonlight",
+    reminder: true,
+  },
+];
+
+module.exports = aboutdata;
--- index.js
+++ index.js
@@ -2,12 +2,17 @@ const express = require("express");
 const axios = require("axios");
 const app = express();
 const cors = require("cors");
-const PORT = 5000;
+const PORT = 4000;
 const database = require("./database.js");
+const aboutdata = require("./aboutdata.js");
 
 app.use(cors());
 
 app.use(express.static("video"));
+
+app.get("/aboutdata", async (req, res, next) => {
+  res.send(aboutdata);
+});
 
 app.get("/", async (req, res, next) => {
   res.send(database);
@@ -17,6 +22,5 @@ app.set("PORT", process.env.PORT || PORT);
 app.listen(app.get("PORT"), (server) => {
   console.info(`Server listen on port ${app.get("PORT")}`);
 });
-
 
 // http://35.226.164.118
